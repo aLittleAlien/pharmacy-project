de317912d79a2a7883106745012b4415249290e7
Biljana <biljana.stanic1@gmail.com>
2012-11-16 00:53:36 +0100

    I added new variables to the class User

--- src/main/java/com/example/pharmacy/entities/User.java
+++ src/main/java/com/example/pharmacy/entities/User.java
@@ -24,68 +24,64 @@ public class User {
 	@Validate("required")
 	@Column(name = "user_password", nullable = false)
 	private String password;
-	@Column(name = "user_gender", nullable = false)
+	@Column(name = "user_gender")
 	private String gender;
-	@Column(name = "user_email", nullable = false)
+	@Column(name = "user_subscribe")
+	private boolean subscribe;
+	@Column(name = "user_email")
 	private String email;
 	@Column(name = "user_country")
 	private String country;
 	@Column(name = "user_age")
 	private double age;
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public String getGender() {
 		return gender;
 	}
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
+	public boolean isSubscribe() {
+		return subscribe;
+	}
+	public void setSubscribe(boolean subscribe) {
+		this.subscribe = subscribe;
+	}
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getCountry() {
 		return country;
 	}
-
 	public void setCountry(String country) {
 		this.country = country;
 	}
-
 	public double getAge() {
 		return age;
 	}
-
 	public void setAge(double age) {
 		this.age = age;
 	}
+	
+	
 }
