2c8d69f56fe4e87047f0c6c3568897e1a162c505
Biljana <biljana.stanic1@gmail.com>
2012-11-16 02:24:50 +0100

    I added code in the class/.tml page Logout and in the .tml page Index

--- src/main/java/com/example/pharmacy/pages/account/Logout.java
+++ src/main/java/com/example/pharmacy/pages/account/Logout.java
@@ -1,5 +1,23 @@
 package com.example.pharmacy.pages.account;
 
+import org.apache.tapestry5.annotations.SessionState;
+
+import com.example.pharmacy.entities.User;
+
+
+
 public class Logout {
+
+	@SessionState
+	private User user;
+
+	public User getUser() {
+		return user;
+	}
+	
+	Object onActivate() {
+		user = null;
+		return null;
+	}
 
 }
