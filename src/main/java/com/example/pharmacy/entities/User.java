2e802e4c461d047be98c32dc706abe5e54c79ada
Biljana <biljana.stanic1@gmail.com>
2012-12-02 13:11:59 +0100

    I formatted the lines of code

--- src/main/java/com/example/pharmacy/entities/User.java
+++ src/main/java/com/example/pharmacy/entities/User.java
@@ -34,54 +34,69 @@ public class User {
 	private String country;
 	@Column(name = "user_age")
 	private double age;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public boolean isSubscribe() {
 		return subscribe;
 	}
+
 	public void setSubscribe(boolean subscribe) {
 		this.subscribe = subscribe;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
 	public double getAge() {
 		return age;
 	}
+
 	public void setAge(double age) {
 		this.age = age;
 	}
-	
-	
+
 }
